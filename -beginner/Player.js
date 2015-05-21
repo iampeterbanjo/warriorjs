7ea3532c31e08c33af86770f66d851e731576beb
iampeterbanjo <peterbanjo@gmail.com>
2015-05-21 15:23:24 +0100

    Can rescue captives

--- -beginner/Player.js
+++ -beginner/Player.js
@@ -1,19 +1,42 @@
 var _underAttack = false
-    , _previousHealth;
+    , _previousHealth
+    , direction = 'backward';
+
+function exploreMind(warrior, health) {
+  if(health < 10) {
+    direction = 'backward';
+  } else {
+    direction = 'forward';
+  }
+
+  warrior.walk(direction);
+}
+
+function aloneMind(warrior, health) {
+  // if under attack don't rest, find the enemy and kill
+  var _underAttack = health > warrior.health();
+
+  if(warrior.health() < 20 && !_underAttack) {
+    warrior.rest();
+  } else {
+    exploreMind(warrior);
+  }
+}
+
+function encounterMind(warrior) {
+  if(warrior.feel(direction).isCaptive()) {
+    warrior.rescue(direction);
+  } else {
+    warrior.attack(direction);
+  }
+}
 
 class Player {
   playTurn(warrior) {
-    // if under attack don't rest, find the enemy and kill
-    _underAttack = _previousHealth > warrior.health();
-
-    if(!warrior.feel().isEmpty()) {
-      warrior.attack();
+    if(!warrior.feel(direction).isEmpty()) {
+      encounterMind(warrior);
     } else {
-      if(warrior.health() < 20 && !_underAttack) {
-        warrior.rest();
-      } else {
-        warrior.walk();
-      }
+      aloneMind(warrior, _previousHealth);
     }
 
     _previousHealth = warrior.health();
